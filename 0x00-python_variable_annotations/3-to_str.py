4daf488920c828baae2e6b1e9459d428ce1e0144
Nick Nyanjui <nicknyanjui@gmail.com>
2023-02-02 23:01:23 +0000

    Add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations on string converter function"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the argument"""
+    return str(n)
