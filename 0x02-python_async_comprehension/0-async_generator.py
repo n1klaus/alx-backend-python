b4374b2e442ba9f6859808242a6ff61cb504daf7
Nick Nyanjui <nicknyanjui@gmail.com>
2023-02-07 21:11:47 +0000

    Add 0-async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import Iterable
+from typing import Generator
 
 
-async def async_generator() -> Iterable[float]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random float between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
