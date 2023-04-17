50bf3572cc595087e48f339e73aa6a61decd6bdb
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-17 09:42:17 +0000

    Update 4-tasks: Use asyncio.as_completed method

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,10 +2,10 @@
 """Running an asyncio Task"""
 
 import asyncio
-import random
 task_wait_random = __import__('3-tasks').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int = 10) -> float:
     """Runs the asyncio task"""
-    return [await task_wait_random(max_delay) for _ in range(n)]
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    return [await coroutine for coroutine in asyncio.as_completed(coroutines)]
