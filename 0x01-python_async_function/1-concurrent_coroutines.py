8a9832c61a764da47b176a9a1b8313fe3d6e641e
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-17 09:35:53 +0000

    Update 1-concurrent_coroutines: Use asyncio.as_completed method

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,10 @@
 """Multiple Asynchronous coroutines at the same time using random delay"""
 
 import asyncio
-import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> float:
-    """Retuns an list of floats after a random delay"""
-    return [await wait_random(max_delay) for _ in range(n)]
+    """Returns a sorted list of floats after a random delay"""
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    return [await coroutine for coroutine in asyncio.as_completed(coroutines)]
