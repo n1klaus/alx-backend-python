0409bfea5f0fee338dc48c69c2bc996fd4463c3f
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-17 09:46:30 +0000

    Update 4-tasks: fix type annotations

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,10 +2,11 @@
 """Running an asyncio Task"""
 
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> float:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Runs the asyncio task"""
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
     return [await coroutine for coroutine in asyncio.as_completed(coroutines)]
