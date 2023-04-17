9e4c6df74c1bb4a120e6e1f23f4e8712e5c3f229
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-17 09:56:16 +0000

    Update 2-measure_runtime: Fix iterable used in gather to use list unpacking with *

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,14 +8,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    Returns the total measured runtime from parallel executions of
+    Returns the total measured runtime from 4 parallel executions of
     async comprehensions
     """
     start = time.perf_counter()
-    await asyncio.gather(
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension())
+    await asyncio.gather(*[async_comprehension()] * 4)
     elapsed = time.perf_counter() - start
     return elapsed
