647428ebda0d69f8212fd7915d42c7875fff4eea
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-17 09:45:27 +0000

    Update 1-concurrent_coroutines: fix type annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """Multiple Asynchronous coroutines at the same time using random delay"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> float:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Returns a sorted list of floats after a random delay"""
     coroutines = [wait_random(max_delay) for _ in range(n)]
     return [await coroutine for coroutine in asyncio.as_completed(coroutines)]
